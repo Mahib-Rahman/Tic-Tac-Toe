f1fbb0237fe77185aef228f07823bc3dc5104c3a
Mahib7009 <50058440+Mahib7009@users.noreply.github.com>
2020-05-14 22:44:50 -0400

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -93,7 +93,9 @@ function reset_game()
         ["7", "8", "9"]
     ];
     if(turn % 2 == 0)
-        turn++;
+        turn = 1;
+    else
+        turn = 0;
     spots_filled = 0;
     one_status = false, two_status = false, three_status = false;
     four_status = false, five_status = false, six_status = false;
